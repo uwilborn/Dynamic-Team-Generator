01956e93790441a821235636385b916bf5aef70b
Ursula Wilborn <wilbornu@yahoo.com>
2021-08-31 22:13:07 -0400

    update html functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -83,7 +84,39 @@ function addIntern() {
 function generateFile() {
   console.log("exit")
   console.log("------------")
+var beginning = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEAM Member from Class</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>`
+var closeHtml = `</body>
+</html>`
+var managerhtml = "";
+for(let i=0;i<myManagerList.length;i++){
+  managerhtml+=`<div class="card bg-primary text-center" style="width: 18rem;">
+  
+  <div class="card-body">
+    <h5 class="card-title">${myManagerList[i].name}</h5>
+    <h6 class="card-title">Manager</h6>
+    <p class="card-text">ID: ${myManagerList[i].id}</p>
+    <p class="card-text">OfficeNumber: ${myManagerList[i].officeNumber}</p>
+    <p class="card-text">Email: ${myManagerList[i].email}</p>
+  </div>
+</div>`
+//Copy from 97-108 and change for engineer and intern
+var HTMLCODE = beginning+managerhtml+closeHtml
+fs.writeFileSync("./main.html",HTMLCODE, (err,data) => {
+  if(err) throw err;
+  console.log("HTML generates")
+})
 }
+}
+
 
 
 

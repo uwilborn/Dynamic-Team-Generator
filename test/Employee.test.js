c7e4fe903635be476de79d625b46371c2b64f5b7
Ursula Wilborn <wilbornu@yahoo.com>
2021-09-02 22:02:15 -0400

    Add test files

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,12 +1,39 @@
+const Employee = require("../lib/Employee");
 const employee = require("../lib/Employee");
 
-describe("Letter class", () => {
-  it("Characters that aren't digits or letters are instantly visible", () => {
-    expect(new Letter("?").visible).toBe(true);
-  });
 
-  it("toString returns _ for letters", () => {
-    expect(new Letter("a").toString()).toBe("_");
-  });
+test("Initiate employee instance",() => {
+  const myEmployee = new Employee;
+  expect(typeof(myEmployee)).toBe("object")
+})
+test("Employee Name checking",() => {
+  const myEmployee = new Employee("Ursula");
+  expect(myEmployee.name).toBe("Ursula")
+})
+test("ID checking",() => {
+  const myEmployee = new Employee("Ursula",45);
+  expect(myEmployee.id).toBe(45)
+})
+test("Email checking",() => {
+  const myEmployee = new Employee("Ursula",45,"ursula@yahoo.com");
+  expect(myEmployee.email).toBe("ursula@yahoo.com")
+})
+test("Check Name Function",() => {
+  const myEmployee = new Employee("Ursula",45,"ursula@yahoo.com");
+  expect(myEmployee.getName()).toBe("Ursula")
+})
+test("Check ID Function",() => {
+  const myEmployee = new Employee("Ursula",45,"ursula@yahoo.com");
+  expect(myEmployee.getId()).toBe(45)
+})
+test("Check Email Function",() => {
+  const myEmployee = new Employee("Ursula",45,"ursula@yahoo.com");
+  expect(myEmployee.getEmail()).toBe("ursula@yahoo.com")
+})
+test("Check Role Function",() => {
+  const myEmployee = new Employee("Ursula",45,"ursula@yahoo.com");
+  expect(myEmployee.getRole()).toBe("Employee")
+})
+
 
   
